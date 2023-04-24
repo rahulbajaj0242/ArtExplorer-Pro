a35d837eae3a709da217a8ac58125c320b8a4d4e
Rahul Bajaj <rahulbajaj0242@gmail.com>
2023-04-24 12:16:12 -0400

    register design updated

--- pages/register.js
+++ pages/register.js
@@ -1,76 +1,175 @@
+import { useState } from 'react';
 import { registerUser } from '@/lib/authenticate';
 import { useRouter } from 'next/router';
-import { Card, Form, Alert, Button } from 'react-bootstrap';
-import { useState } from 'react';
+import {
+  Card,
+  FormControl,
+  InputLabel,
+  Input,
+  Button,
+  Typography,
+  Alert,
+  makeStyles,
+} from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    // backgroundColor: '#f2f2f2',
+    padding: theme.spacing(2),
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    minHeight: '80vh',
+  },
+  form: {
+    maxWidth: '400px',
+    minHeight: '600px',
+    width: '100%',
+    padding: '32px',
+    borderRadius: '8px',
+    boxShadow: '0px 2px 8px rgba(0, 0, 0, 0.1)',
+  },
+  formTitle: {
+    marginBottom: '16px',
+    fontWeight: 'bold',
+    fontSize: '24px',
+    textAlign: 'center',
+  },
+  formText: {
+    marginBottom: '16px',
+    fontWeight: 'normal',
+    fontSize: '16px',
+    textAlign: 'center',
+  },
+  formControl: {
+    marginBottom: theme.spacing(2),
+    '& label.Mui-focused': {
+      color: '#0070f3',
+    },
+    '& .MuiInput-underline:after': {
+      borderBottomColor: '#0070f3',
+    },
+    '& .MuiOutlinedInput-root': {
+      '&.Mui-focused fieldset': {
+        borderColor: '#0070f3',
+      },
+    },
+  },
+  formButton: {
+    marginTop: '16px',
+    borderRadius: '4px',
+    height: '40px',
+    fontSize: '16px',
+    fontWeight: 'bold',
+    textTransform: 'none',
+  },
+  formAlert: {
+    marginTop: '16px',
+  },
+}));
 
 export default function Register(props) {
   const [user, setUser] = useState('');
   const [password, setPassword] = useState('');
-  const [password2, setPassword2] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
   const [warning, setWarning] = useState('');
   const router = useRouter();
+  const classes = useStyles();
 
-  async function handleSubmit(e) {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    if (password !== confirmPassword) {
+      setWarning('Passwords do not match');
+      return;
+    }
     try {
-      await registerUser(user, password, password2);
+      await registerUser(user, password, confirmPassword);
       router.push('/login');
     } catch (err) {
       setWarning(err.message);
     }
-  }
+  };
 
   return (
-    <>
-      {warning && (
-        <>
-          <br />
-          <Alert variant="danger">{warning}</Alert>
-        </>
-      )}
-      <Card bg="light">
-        <Card.Body>
-          <h2>Register</h2>Register for an account:
-        </Card.Body>
-      </Card>
-      <br />
-      <Form onSubmit={handleSubmit}>
-        <Form.Group>
-          <Form.Label>User:</Form.Label>
-          <Form.Control
-            type="text"
-            value={user}
-            id="userName"
-            name="userName"
-            onChange={(e) => setUser(e.target.value)}
-          />
-        </Form.Group>
-        <br />
-        <Form.Group>
-          <Form.Label>Password:</Form.Label>
-          <Form.Control
-            type="password"
-            value={password}
-            id="password"
-            name="password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Confirm Password:</Form.Label>
-          <Form.Control
-            type="password"
-            value={password2}
-            id="password2"
-            name="password2"
-            onChange={(e) => setPassword2(e.target.value)}
-          />
-        </Form.Group>
-        <br />
-        <Button variant="primary" className="pull-right" type="submit">
+    <div className={classes.root}>
+      <Card variant="outlined" className={classes.form}>
+        <Typography variant="h5" component="h2" className={classes.formTitle}>
           Register
-        </Button>
-      </Form>
-    </>
+        </Typography>
+        <Typography
+          color="textSecondary"
+          gutterBottom
+          className={classes.formText}
+        >
+          Enter your registration information below:
+        </Typography>
+        <form onSubmit={handleSubmit}>
+          <FormControl
+            fullWidth
+            margin="normal"
+            className={classes.formControl}
+          >
+            <InputLabel htmlFor="userName">User</InputLabel>
+            <Input
+              id="userName"
+              name="userName"
+              value={user}
+              onChange={(e) => setUser(e.target.value)}
+              autoFocus
+              required
+              autoComplete="username"
+              inputProps={{ 'aria-label': 'user name' }}
+            />
+          </FormControl>
+          <FormControl
+            fullWidth
+            margin="normal"
+            className={classes.formControl}
+          >
+            <InputLabel htmlFor="password">Password</InputLabel>
+            <Input
+              id="password"
+              name="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              autoComplete="new-password"
+              inputProps={{ 'aria-label': 'password' }}
+            />
+          </FormControl>
+          <FormControl
+            fullWidth
+            margin="normal"
+            className={classes.formControl}
+          >
+            <InputLabel htmlFor="confirmPassword">Confirm Password</InputLabel>
+            <Input
+              id="confirmPassword"
+              name="confirmPassword"
+              type="password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              required
+              autoComplete="new-password"
+              inputProps={{ 'aria-label': 'confirm password' }}
+            />
+          </FormControl>
+          {warning && (
+            <Alert severity="error" className={classes.formAlert}>
+              {warning}
+            </Alert>
+          )}
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            className={classes.formButton}
+          >
+            Register
+          </Button>
+        </form>
+      </Card>
+    </div>
   );
 }
