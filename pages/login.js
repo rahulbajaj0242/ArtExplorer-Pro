469ab49009950a225c2553af83cc4f48be3d08a8
Rahul Bajaj <rahulbajaj0242@gmail.com>
2023-04-24 12:22:43 -0400

    code updated but fails one test when password wrng

--- pages/login.js
+++ pages/login.js
@@ -1,21 +1,88 @@
+import { useState } from 'react';
 import { useAtom } from 'jotai';
 import { searchHistoryAtom } from '@/store';
 import { favouritesAtom } from '@/store';
 import { getHistory, getFavourites } from '@/lib/userData';
 import { authenticateUser } from '@/lib/authenticate';
 import { useRouter } from 'next/router';
-import { Card, Form, Alert, Button } from 'react-bootstrap';
-import { useState } from 'react';
+import {
+  Card,
+  FormControl,
+  FormHelperText,
+  InputLabel,
+  Input,
+  Button,
+  Typography,
+  Alert,
+  makeStyles,
+} from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    // backgroundColor: '#f2f2f2',
+    padding: theme.spacing(2),
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    minHeight: '80vh',
+  },
+  form: {
+    maxWidth: '400px',
+    minHeight: '600px',
+    width: '100%',
+    padding: '32px',
+    borderRadius: '8px',
+    boxShadow: '0px 2px 8px rgba(0, 0, 0, 0.1)',
+  },
+  formTitle: {
+    marginBottom: '16px',
+    fontWeight: 'bold',
+    fontSize: '24px',
+    textAlign: 'center',
+  },
+  formText: {
+    marginBottom: '16px',
+    fontWeight: 'normal',
+    fontSize: '16px',
+    textAlign: 'center',
+  },
+  formControl: {
+    marginBottom: theme.spacing(2),
+    '& label.Mui-focused': {
+      color: '#0070f3',
+    },
+    '& .MuiInput-underline:after': {
+      borderBottomColor: '#0070f3',
+    },
+    '& .MuiOutlinedInput-root': {
+      '&.Mui-focused fieldset': {
+        borderColor: '#0070f3',
+      },
+    },
+  },
+  formButton: {
+    marginTop: '16px',
+    borderRadius: '4px',
+    height: '40px',
+    fontSize: '16px',
+    fontWeight: 'bold',
+    textTransform: 'none',
+  },
+  formAlert: {
+    marginTop: '16px',
+  },
+}));
 
 export default function Login(props) {
   const [user, setUser] = useState('');
   const [password, setPassword] = useState('');
   const [warning, setWarning] = useState('');
   const [searchHistory, setSearchHistory] = useAtom(searchHistoryAtom);
-  const [faviouritesList, setFavouritesList] = useAtom(favouritesAtom);
+  const [favouritesList, setFavouritesList] = useAtom(favouritesAtom);
   const router = useRouter();
+  const classes = useStyles();
 
-  async function handleSubmit(e) {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       await authenticateUser(user, password);
@@ -24,54 +91,79 @@ export default function Login(props) {
     } catch (err) {
       setWarning(err.message);
     }
-  }
+  };
 
-  async function updateAtoms() {
+  const updateAtoms = async () => {
     setFavouritesList(await getFavourites());
     setSearchHistory(await getHistory());
-  }
+  };
 
   return (
-    <>
-      {warning && (
-        <>
-          <br />
-          <Alert variant="danger">{warning}</Alert>
-        </>
-      )}
-      <Card bg="light">
-        <Card.Body>
-          <h2>Login</h2>Enter your login information below:
-        </Card.Body>
-      </Card>
-      <br />
-      <Form onSubmit={handleSubmit}>
-        <Form.Group>
-          <Form.Label>User:</Form.Label>
-          <Form.Control
-            type="text"
-            value={user}
-            id="userName"
-            name="userName"
-            onChange={(e) => setUser(e.target.value)}
-          />
-        </Form.Group>
-        <br />
-        <Form.Group>
-          <Form.Label>Password:</Form.Label>
-          <Form.Control
-            type="password"
-            value={password}
-            id="password"
-            name="password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </Form.Group>
-        <br />
-        <Button variant="primary" className="pull-right" type="submit">
+    <div className={classes.root}>
+      <Card variant="outlined" className={classes.form}>
+        <Typography variant="h5" component="h2" className={classes.formTitle}>
           Login
-        </Button>
-      </Form>
-    </>
+        </Typography>
+        <Typography
+          color="textSecondary"
+          gutterBottom
+          className={classes.formText}
+        >
+          Enter your login information below:
+        </Typography>
+        <form onSubmit={handleSubmit}>
+          <FormControl
+            fullWidth
+            margin="normal"
+            className={classes.formControl}
+          >
+            <InputLabel htmlFor="userName">User</InputLabel>
+            <Input
+              id="userName"
+              name="userName"
+              value={user}
+              onChange={(e) => setUser(e.target.value)}
+              autoFocus
+              required
+              autoComplete="username"
+              inputProps={{ 'aria-label': 'user name' }}
+            />
+          </FormControl>
+          <FormControl
+            fullWidth
+            margin="normal"
+            className={classes.formControl}
+          >
+            <InputLabel htmlFor="password">Password</InputLabel>
+            <Input
+              id="password"
+              name="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              autoComplete="current-password"
+              inputProps={{ 'aria-label': 'password' }}
+            />
+          </FormControl>
+          <Button
+            variant="contained"
+            color="primary"
+            type="submit"
+            className={classes.formButton}
+          >
+            Login
+          </Button>
+        </form>
+        {warning && (
+          <>
+            <br />
+            <Alert severity="error" className={classes.formAlert}>
+              {warning}
+            </Alert>
+          </>
+        )}
+      </Card>
+    </div>
   );
 }
