ffc92ac4db4d2b675c6ada0b8637b3b01f6f1a41
Rahul Bajaj <rahulbajaj0242@gmail.com>
2023-04-24 14:39:57 -0400

    front page completed

--- pages/index.js
+++ pages/index.js
@@ -1,62 +1,81 @@
-import Image from 'react-bootstrap/Image';
-import { Row, Col } from 'react-bootstrap';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Button, Container, Grid, Typography } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  hero: {
+    position: 'relative' /* add this to make the pseudo-element work */,
+    backgroundImage: `url(https://images.unsplash.com/photo-1572953109213-3be62398eb95?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    height: 'calc(100vh - 64px)',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: '#f5f5f5' /* a lighter shade of white */,
+    padding: theme.spacing(4),
+    [theme.breakpoints.down('sm')]: {
+      height: '50vh',
+    },
+  },
+
+  title: {
+    fontWeight: 'bold',
+    fontSize: '48px',
+    marginBottom: theme.spacing(2),
+    [theme.breakpoints.down('sm')]: {
+      fontSize: '36px',
+    },
+  },
+  subtitle: {
+    fontSize: '24px',
+    marginBottom: theme.spacing(4),
+    [theme.breakpoints.down('sm')]: {
+      fontSize: '18px',
+    },
+  },
+  button: {
+    borderRadius: '4px',
+    height: '48px',
+    fontSize: '18px',
+    fontWeight: 'bold',
+    textTransform: 'none',
+    backgroundColor: theme.palette.primary.main,
+    color: 'white',
+    '&:hover': {
+      backgroundColor: theme.palette.primary.dark,
+    },
+  },
+}));
 
 export default function Home() {
+  const classes = useStyles();
+
   return (
     <>
-      <Image
-        src="https://upload.wikimedia.org/wikipedia/commons/3/30/Metropolitan_Museum_of_Art_%28The_Met%29_-_Central_Park%2C_NYC.jpg 
-"
-        fluid
-        rounded
-        alt="image of museum"
-      ></Image>
-      <br />
-      <br />
-      <Row>
-        <Col md={6}>
-          <p>
-            The Metropolitan Museum of Art of New York City, colloquially
-            &quot;the Met&quot;, is the largest art museum in the Americas. Its
-            permanent collection contains over two million works, divided among
-            17 curatorial departments. The main building at 1000 Fifth Avenue,
-            along the Museum Mile on the eastern edge of Central Park on
-            Manhattan&apos;s Upper East Side, is by area one of the world&apos;s
-            largest art museums. A much smaller second location, The Cloisters
-            at Fort Tryon Park in Upper Manhattan, contains an extensive
-            collection of art, architecture, and artifacts from medieval Europe.
-          </p>
-          <p>
-            The Metropolitan Museum of Art was founded in 1870 with its mission
-            to bring art and art education to the American people. The
-            museum&apos;s permanent collection consists of works of art from
-            classical antiquity and ancient Egypt, paintings, and sculptures
-            from nearly all the European masters, and an extensive collection of
-            American and modern art. The Met maintains extensive holdings of
-            African, Asian, Oceanian, Byzantine, and Islamic art. The museum is
-            home to encyclopedic collections of musical instruments, costumes,
-            and accessories, as well as antique weapons and armor from around
-            the world. Several notable interiors, ranging from 1st-century Rome
-            through modern American design, are installed in its galleries.
-          </p>
-        </Col>
-        <Col md={6}>
-          The Fifth Avenue building opened on March 30, 1880. In 2021, despite
-          the COVID-19 pandemic in New York City, the museum attracted 1,958,000
-          visitors, ranking fourth on the list of most-visited art museums in
-          the world.
-          <br />
-          <br />
-          <a
-            href="https://en.wikipedia.org/wiki/Metropolitan_Museum_of_Art"
-            target="_blank"
-            rel="noreferrer"
+      <div className={classes.hero}>
+        <Container maxWidth="md">
+          <Typography variant="h1" component="h1" className={classes.title}>
+            Discover Artworks and Artifacts from Around the World
+          </Typography>
+          <Typography variant="h4" component="h2" className={classes.subtitle}>
+            Search by location, time period, and characteristics
+          </Typography>
+          <Button
+            variant="contained"
+            color="primary"
+            className={classes.button}
+            href="/search"
           >
-            https://en.wikipedia.org/wiki/Metropolitan_Museum_of_Art
-          </a>
-          <br />
-        </Col>
-      </Row>
+            Search Now
+          </Button>
+        </Container>
+      </div>
+      <Container maxWidth="md">
+        <Grid container spacing={4}>
+          {/* Add more content here, such as featured artworks or artifacts */}
+        </Grid>
+      </Container>
     </>
   );
 }
